a63b2cd96a4d840726634d9721c6c43170061590
hechen2 <hechen2@yy.com>
2024-04-09 17:23:18 +0800

    fast 1.0.0

---
+++ fastenv.rb
@@ -0,0 +1,32 @@
+class Fastenv < Formula
+  desc "A simple 'fast init env' program"
+  homepage "https://github.com/anathann/fastenv"
+  url "https://github.com/anathann/fastenv/releases/download/v1.0.0/fastenv.tar"
+  sha256 "a2243c1e71a57d164913f2047b6292fdd3accb67d729a25ea2163081caed6cdd"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "fastenv"
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test fastenv`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
